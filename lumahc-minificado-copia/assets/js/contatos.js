3c478818ec702e4b27336760af62d7e85544e72a
PxS00 <lucas.rossonidd@gmail.com>
2025-08-18 20:01:42 -0300

    fix: removida cópia indevida do projeto dentro dele mesmo (erro de estrutura)

--- lumahc-minificado-copia/assets/js/contatos.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const contatos = document.querySelectorAll('.contato h3');
-
-  contatos.forEach((el) => {
-    el.addEventListener('click', () => {
-      el.parentElement.classList.toggle('active');
-    });
-  });
-});
