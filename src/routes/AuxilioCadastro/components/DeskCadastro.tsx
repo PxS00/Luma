19a06bd3cbf56597f02a9e74bfc3b8ec72a80aa6
PxS00 <lucas.rossonidd@gmail.com>
2025-08-21 21:17:16 -0300

    feat: adicionar DeskCadastro, MobileCadastro e página Auxilio Cadastro; Adiciona arquivo data com a definição de todos os passos; Configura rotas para acesso à página de auxílio ao cadastro

---
+++ src/routes/AuxilioCadastro/components/DeskCadastro.tsx
@@ -0,0 +1,25 @@
+import BtnExterno from '../../../components/Botao/BtnExterno';
+import ListaPassos from '../../../components/Passos/ListaPassos';
+import { PASSOS_DESK } from '../data/passosCadastro';
+
+export default function DeskCadastro() {
+  return (
+    <section className="desk-cadastro">
+      <div className="intro">
+        <h2>Acesse pelo Navegador</h2>
+        <p>Você também pode fazer seu cadastro diretamente pelo site, sem instalar nada.</p>
+
+        <BtnExterno href="https://portaldopaciente.hc.fm.usp.br/" target="_blank">
+          Acessar Site
+        </BtnExterno>
+      </div>
+
+      <ListaPassos
+        titulo="Como fazer o cadastro no navegador"
+        passos={PASSOS_DESK}
+        mostrarControles
+        className="lista-passos" //! hook p/ Tailwind 
+      />
+    </section>
+  );
+}
---
+++ src/routes/AuxilioCadastro/components/MobileCadastro.tsx
@@ -0,0 +1,26 @@
+import BtnExterno from "../../../components/Botao/BtnExterno";
+import ListaPassos from "../../../components/Passos/ListaPassos";
+import { PASSOS_MOBILE, STORE_LINKS } from "../data/passosCadastro";
+
+export default function MobileCadastro() {
+  return (
+    <section>
+          <div>
+            <h2>Baixe o App</h2>
+            <p>
+              Para uma experiência mais completa, recomendamos baixar o Portal do Paciente HC.
+            </p>
+            <div>
+              <BtnExterno href={STORE_LINKS.play.href} target="_blank">
+                <img src={STORE_LINKS.play.icon} alt={STORE_LINKS.play.alt} width={20} height={20} /> Baixar na Play Store
+              </BtnExterno>{' '}
+              <BtnExterno href={STORE_LINKS.app.href} target="_blank">
+                <img src={STORE_LINKS.app.icon} alt={STORE_LINKS.app.alt} width={20} height={20} /> Baixar na App Store
+              </BtnExterno>
+            </div>
+          </div>
+    
+          <ListaPassos titulo="Como usar no App" passos={PASSOS_MOBILE} mostrarControles />
+        </section>
+  );
+}
