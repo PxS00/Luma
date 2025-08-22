3bfd70feae6220734d793ccae925513066e2b2a6
KelsonZh0 <kel180498@gmail.com>
2025-08-21 22:55:59 -0300

    refatorar: melhorar o estilo e a estrutura do FaqItem para melhor legibilidade

--- src/pages/Faq/components/FaqItem.tsx
+++ src/pages/Faq/components/FaqItem.tsx
@@ -5,15 +5,17 @@ type Props = { item: FaqData };
 
 export default function FaqItem({ item }: Props) {
   return (
-    <div className='faq-item'>
-      <h3>{item.pergunta}</h3>
-      <p>
+    <div className=' mb-4 rounded-[12px] border border-borderColor bg-backPrimary transition-colors duration-300 ease-in-out'>
+      <h3 className='py-2.5 px-3.5 m-0 text-center text-[18px]
+          cursor-pointer text-clikColor font-bold
+          transition-[font-size]'>{item.pergunta}</h3>
+      <p className=' rounded-b-[12px] py-2.5 px-3.5 m-0 bg-[#FFF8E1] text-fontTertiary border-t border-borderColor text-base'>
         {item.resposta}
         {item.link && (
           <>
             {' '}
-            <BtnExterno href={item.link} target='_blank' >
-              Saiba mais
+            <BtnExterno href={item.link} target='_blank' className='px-2 py-1 text-[10px] bg-[#FFF8E1] ' >
+              Teleconsulta
             </BtnExterno>
           </>
         )}
--- src/pages/Faq/index.tsx
+++ src/pages/Faq/index.tsx
@@ -3,13 +3,13 @@ import FaqItem from './components/FaqItem';
 
 export default function Faq() {
   return (
-    <main>
-      <div className='container'>
-        <h1 className='faq-title'>Perguntas Frequentes (FAQ)</h1>
-        <p className='faq-subtitle'>
+    <main className='ml-0 p-5 w-full flex-1'>
+      <div className=' bg-backSecondary p-5 rounded-[10px] max-w-full m-auto shadow-[0_2px_6px_rgba(255,112,67,0.25)] box-border relative'>
+        <h1 className='text-fontPrimary text-2xl mb-2.5 text-center'>Perguntas Frequentes (FAQ)</h1>
+        <p className='text-base text-fontSecondary mb-5 text-center'>
           Encontre respostas rápidas sobre o uso da plataforma de Saúde Digital.
         </p>
-        <div className='faq-section'>
+        <div className='mt-5'>
           {faqData.map((item, idx) => (
             <FaqItem key={idx} item={item} />
           ))}
