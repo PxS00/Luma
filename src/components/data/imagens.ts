065c273adf82aa98e51204c0ae48f5e766b766d7
PxS00 <lucas.rossonidd@gmail.com>
2025-08-21 21:03:38 -0300

    refactor: reorganizar estrutura de rotas e remover componentes não utilizados

---
+++ src/components/data/imagens.ts
@@ -0,0 +1,56 @@
+//! Avatares
+export { default as user } from '../../img/avatares/user.png';
+export { default as users } from '../../img/avatares/users.png';
+export { default as celular } from '../../img/avatares/celular.png';
+export { default as pessoa } from '../../img/avatares/pessoa.png';  
+export { default as logo } from '../../img/logo/lumahc.png';
+
+//! Ícones
+export { default as playstore } from '../../img/icones/playstore.png';
+export { default as appstore } from '../../img/icones/app-store.png';
+export { default as GitHub} from "../../img/icones/github.png"
+export { default as LinkedIn} from "../../img/icones/linkedin.png"
+
+//! Passo a passo
+//* Iniciais
+export { default as instalarPlaystore } from '../../img/passo-a-passo/instalar-playstore.jpg';
+export { default as abrindoApp } from '../../img/passo-a-passo/abrindo-app.jpg';
+export { default as acessandoApp } from '../../img/passo-a-passo/acessando-app.jpg';
+export { default as entrandoApp } from '../../img/passo-a-passo/entrando-app.jpg';
+export { default as acessoPortal } from '../../img/passo-a-passo/acessando-portal.png';        
+export { default as acessoNav } from '../../img/passo-a-passo/acesso-nav.png'; 
+
+//* Navegação dentro do app/portal
+export { default as acessandoMenu } from '../../img/passo-a-passo/acessando-menu.jpg';
+export { default as teleconsulta } from '../../img/passo-a-passo/teleconsulta.jpg';
+
+//* Termo / confirmação
+export { default as direcionamentoTermo } from '../../img/passo-a-passo/direcionamento-termo.jpg';
+export { default as aceitandoTermo } from '../../img/passo-a-passo/aceitando-termo.jpg';
+export { default as detalhesTeleconsulta } from '../../img/passo-a-passo/detalhes-teleconsulta.jpg';
+
+//* Cadastro    
+export { default as cadastroSenha } from '../../img/passo-a-passo/cadastro-senha.jpg';
+export { default as cadastrando } from '../../img/passo-a-passo/cadastrando.png';           
+export { default as confirmacaoAno } from '../../img/passo-a-passo/confirmacao-ano.jpg';
+export { default as confirmacaoNomeMae } from '../../img/passo-a-passo/confirmacao-nome-mae.jpg';
+export { default as dadosContato } from '../../img/passo-a-passo/dados-contato.png';          
+export { default as preenchendoCpf } from '../../img/passo-a-passo/preenchimento-cpf.jpg';
+export { default as senhaCadastro } from '../../img/passo-a-passo/senha-cadastro.jpg';
+export { default as verificacaoDados } from '../../img/passo-a-passo/verificacao-dados.png';
+
+//* Seleção de navegador / permissões
+export { default as selecNavegador } from '../../img/passo-a-passo/selecionando-navegador.jpg';
+export { default as autorizandoAudio } from '../../img/passo-a-passo/autorizando-audio.png';
+export { default as liberandoCamera } from '../../img/passo-a-passo/liberando-camera.jpg';
+export { default as permissaoCamera } from '../../img/passo-a-passo/permissao-camera.jpg';
+export { default as opcoesCamera } from '../../img/passo-a-passo/opcoes-camera.jpg';
+
+//* Encerramento
+export { default as finalTeleconsulta } from '../../img/passo-a-passo/final-teleconsulta.jpg';
+
+//! Integrantes
+export { default as Alex} from "../../img/integrantes/alexander.jpg"
+export { default as Kelson} from "../../img/integrantes/kelson-zhang.jpg"
+export { default as Lucas} from "../../img/integrantes/lucas-rossoni.png"
+
