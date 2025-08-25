0e5d61bffaee5b3d8911bb2bba5164d9efead3cd
PxS00 <lucas.rossonidd@gmail.com>
2025-08-25 13:28:58 -0300

    feat: refatorar ContatoItem para usar ToggleSection

--- src/components/contato/ContatoItem.tsx
+++ src/components/contato/ContatoItem.tsx
@@ -1,32 +1,58 @@
 import type { Contato } from '@/types/contato';
 import BtnExterno from '../Botao/BtnExterno';
+import ToggleSection from '../ToggleSection/ToggleSection';
 
 type Props = { item: Contato };
 
 export default function ContatoItem({ item }: Props) {
   return (
     <div className='contato'>
-      <h3>{item.titulo}</h3>
-
-      {item.presencial && <p>Presencial: {item.presencial}</p>}
-
-      {item.email && (
-        <p>
-          E-mail: <a href={`mailto:${item.email}`}>{item.email}</a>
-        </p>
-      )}
-
-      {item.tel && <p>Tel: {item.tel}</p>}
-
-      {item.funcionamento && <p>Funcionamento: {item.funcionamento}</p>}
-
-      {item.linkExterno && (
-        <p>
-          <BtnExterno href={item.linkExterno.href} target='_blank'>
-            {item.linkExterno.rotulo}
-          </BtnExterno>
-        </p>
-      )}
+      <ToggleSection title={item.titulo}>
+        <dl className='space-y-2'>
+          {item.presencial && (
+            <div>
+              <dt className='font-semibold'>Presencial:</dt>
+              <dd>{item.presencial}</dd>
+            </div>
+          )}
+          {item.email && (
+            <div>
+              <dt className='font-semibold'>E-mail:</dt>
+              <dd>
+                <BtnExterno href={`mailto:${item.email}`} target='_blank'>
+                  {item.email}
+                </BtnExterno>
+              </dd>
+            </div>
+          )}
+          {item.tel && (
+            <div>
+              <dt className='font-semibold'>E-mail:</dt>
+              <dd>
+                <BtnExterno href={`mailto:${item.tel}`} target='_blank'>
+                  {item.tel}
+                </BtnExterno>
+              </dd>
+            </div>
+          )}
+          {item.funcionamento && (
+            <div>
+              <dt className='font-semibold'>Funcionamento:</dt>
+              <dd>{item.funcionamento}</dd>
+            </div>
+          )}
+          {item.linkExterno && (
+            <div>
+              <dt className='sr-only'>Link externo</dt>
+              <dd>
+                <BtnExterno href={item.linkExterno.href} target='_blank'>
+                  {item.linkExterno.rotulo}
+                </BtnExterno>
+              </dd>
+            </div>
+          )}
+        </dl>
+      </ToggleSection>
     </div>
   );
 }
